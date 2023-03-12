711f0a941097a39973b97bc811eaabf27986ed2a
prasannar <rodrigo.rajitha@gmail.com>
2023-03-12 10:28:39 +0530

    add video

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -376,7 +376,7 @@ $(function(){
           markup: '<div class="mfp-iframe-scaler">'+
                     '<div class="mfp-close"></div>'+
                     '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-                  '</div>', 
+                  '</div>',
 
           patterns: {
             youtube: {
